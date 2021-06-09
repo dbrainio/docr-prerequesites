4de65614bab9828eb0bd0cd5c9cacc485575f551
Aynur Maksutov <maksutov_aynur@mail.ru>
2021-06-09 14:35:24 +0300

    ok

--- debian/install.sh
+++ debian/install.sh
@@ -8,6 +8,9 @@ sudo apt-get install -y \
     gnupg \
     lsb-release
 
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 echo \
   "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
